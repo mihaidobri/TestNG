80c77b5c93ba5d57ef9ac6eed6ca903cfafb5bf0
mdobri <dobrimihai@yahoo.com>
2017-09-20 12:00:25 +0300

    dummy test

--- src/appcode/SomeClassToTest.java
+++ src/appcode/SomeClassToTest.java
@@ -4,15 +4,17 @@ public class SomeClassToTest {
 	
 	public int sumNumber(int a, int b){
 		return a+b;
+		
 	}
 
 	public String addStrings(String a,String b){
-		return a + " " + b;
+		return a + " test" + b;
 	}
 	
 	public int[] getArray(){
 		int[] arrayExample = {1,2,3};
 		return arrayExample;
+	
 	
 	}
 	
